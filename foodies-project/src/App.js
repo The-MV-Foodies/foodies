8b4ff1f9cf46cdee725c4556d7899ad66ee48376
Brianna Gaines <bgaines@cisco.com>
2023-01-27 12:41:12 -0800

    adds starter code to App.js

--- foodies-project/src/App.js
+++ foodies-project/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'ab83417137msh9e6c2eae529dabbp1bac36jsne2f4ef37b89a',
+      'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+    }
+  };
+
+  async function fetchItems(){
+		try {
+			const response = await fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes', options)
+			const recipesData = await response.json();
+      console.log(recipesData.results)
+			
+			setRecipes(recipesData.results);
+		} catch (err) {
+			console.log("Oh no an error! ", err)
+		}
+	}
+
+	useEffect(() => {
+		fetchItems()
+	}, []);
+   
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='cards'> 
+    {recipes.map((singleRecipe, idx) => {
+      return(
+         <> 
+       <div className='card' key={idx}> 
+        <img style={{width: "200px", height: "300px"}} className='card-image' src={singleRecipe.thumbnail_url} alt={singleRecipe.name} />  
+        </div> 
+        </>
+  ) } )} 
+      
+   </div>
   );
 }
 
