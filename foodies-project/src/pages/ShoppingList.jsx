07d8895ce5c6a39ae0b8e34f9a6603e62fe0623e
Brianna Gaines <bgaines@cisco.com>
2023-02-10 13:31:41 -0800

    adds local storage to shopping list

--- foodies-project/src/pages/ShoppingList.jsx
+++ foodies-project/src/pages/ShoppingList.jsx
@@ -6,18 +6,30 @@ import { Col, Container, Row } from 'react-bootstrap';
 
 
 function ShoppingList (){
-    const { shoppingList } = useContext(ShoppingListContext);
+    const { shoppingList, removeFromShoppingList } = useContext(ShoppingListContext);
     console.log(shoppingList)
+    let newList = JSON.parse(localStorage.getItem("shoppingList"))
+    console.log("newlist: ",newList)
+    console.log("shoppingList: ",shoppingList)
+    function deleteItem(i){
+        console.log("i is: ", i)
+        newList.splice(newList.indexOf(i), 1);
+        localStorage.setItem("shoppingList", JSON.stringify(newList));
+        removeFromShoppingList(i)
+        };
+
     return(
         <>
+        <h1 className="text-center">Shopping List</h1>
         <Container>
         <Row className='g-4'>
-        {shoppingList.map((listItem)=>(
-            <Col key={listItem}>
-            <h3>{listItem}</h3>
-            </Col>
-       
+        {newList.map((listItem)=>(
+            <div className="d-inline-flex">
+                <button style={{height:"10px", paddingBottom:"30px"}} className="btn btn-danger btn-sm text-center" onClick={()=> deleteItem(listItem)}>-</button>
+                <h3>{listItem}</h3>
+            </div>
         ))}
+     
         </Row>
         </Container>
         </>
