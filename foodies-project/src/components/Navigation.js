dcb42b65f823a2d64dca84ab7c35fae14ff5211d
Ana Gonzalez Hiraldo <gonzaana35@gmail.com>
2023-02-17 17:02:36 -0500

    changed the card sizes

--- foodies-project/src/components/Navigation.js
+++ foodies-project/src/components/Navigation.js
@@ -8,19 +8,27 @@ import Image from 'react-bootstrap/Image';
 
 function Navigation() {
   return (
-    <Navbar sticky='top' bg='dark' variant='dark' className='mb-4'>
+    <Navbar sticky='top' bg='light' variant='dark' className='mb-4'>
       <Container>
         {/* <Navbar.Brand>
           <Image src={squirtle} width='30' className='me-2' />
           Pokeverse
         </Navbar.Brand> */}
 
-        <Nav className='me-auto'>
-          <Nav.Link as="div">
-            <NavLink to='/'>All Recipes</NavLink>
-            <NavLink to='/shoppingList'>My Shopping List</NavLink>
-          </Nav.Link>
-        </Nav>
+        {/* <Nav className="nav justify-content-end">
+           <Nav.Link as="div">
+            <NavLink to='/' style={{ textDecoration: 'none' }}>All Recipes</NavLink>
+            <NavLink to='/shoppingList' style={{ textDecoration: 'none' }}>My Shopping List</NavLink>
+          </Nav.Link>  */}
+           <ul class="nav justify-content-end">
+              <li class="nav-item">
+                 <a class="nav-link" href="#">All Recipes</a>
+              </li>
+              <li class="nav-item">
+                 <a class="nav-link" href="#">Shopping List</a>
+              </li> 
+           </ul> 
+        {/* </Nav> */}
       </Container>
     </Navbar>
   );
