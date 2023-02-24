9a4ef91244df0f375ec1bdd4472e3d1c953b752e
Ana Gonzalez Hiraldo <gonzaana35@gmail.com>
2023-02-24 15:11:28 -0500

    I took the search bar off the nav bar

--- foodies-project/src/components/Navigation.js
+++ foodies-project/src/components/Navigation.js
@@ -9,33 +9,7 @@ import Image from 'react-bootstrap/Image';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
-function Navigation() {
-//   return (
-//     <Navbar sticky='top' bg='light' variant='dark' className='mb-4'>
-//       <Container>
-//         {/* <Navbar.Brand>
-//           <Image src={squirtle} width='30' className='me-2' />
-//           Pokeverse
-//         </Navbar.Brand> */}
-
-//         {/* <Nav className="nav justify-content-end">
-//            <Nav.Link as="div">
-//             <NavLink to='/' style={{ textDecoration: 'none' }}>All Recipes</NavLink>
-//             <NavLink to='/shoppingList' style={{ textDecoration: 'none' }}>My Shopping List</NavLink>
-//           </Nav.Link>  */}
-//            <ul class="nav justify-content-end">
-//               <li class="nav-item">
-//                  <a class="nav-link" href="#">All Recipes</a>
-//               </li>
-//               <li class="nav-item">
-//                  <a class="nav-link" href="#">Shopping List</a>
-//               </li> 
-//            </ul> 
-//         {/* </Nav> */}
-//       </Container>
-//     </Navbar>
-//   );
-// }
+function Navigation() {   
 
 return (
   <Navbar bg="light" expand="lg">
@@ -64,15 +38,7 @@ return (
             
           </Nav.Link>
         </Nav>
-        <Form className="d-flex">
-          <Form.Control
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-          />
-          <Button variant="outline-success">Search</Button>
-        </Form>
+        
       </Navbar.Collapse>
     </Container>
   </Navbar>
